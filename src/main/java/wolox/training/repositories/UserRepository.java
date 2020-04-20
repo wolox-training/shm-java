4b30a0e4e6c0625d4e2687334be2b087a601ee08
shernanm <santiago.hernandez@wolox.co>
2020-04-20 10:36:00 -0500

    added user repository

---
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package wolox.training.repositories;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import wolox.training.models.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByUserName(String userName);
+}
